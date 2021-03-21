60285c1dd8e8ce9db9f6539d3fad277a986ac791
Patrick Lewis <Patrick.Lewis@modusbox.com>
2021-03-21 12:46:58 -0700

    chore(MOD): add additional mobs to gain honor

--- data/sql/custom/db_world/creature_template.sql
+++ data/sql/custom/db_world/creature_template.sql
@@ -1,1 +1,1 @@
-UPDATE `creature_template` SET `flags_extra` = '32768' WHERE `entry` IN (30354,30355);
+UPDATE `creature_template` SET `flags_extra` = '32768' WHERE `entry` IN (30354,30355,30739,30740);
